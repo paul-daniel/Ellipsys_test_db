7a01e189e332f14422b7d32c8e1cfa95c1eb4d59
paul-daniel <dantchatchouang@gmail.com>
2023-08-31 15:54:17 +0200

    add fill reduded table method

--- index.js
+++ index.js
@@ -3,6 +3,29 @@ const db = new sqlite3.Database('ellipsys_test_db.db3')
 
 const SOURCE_TABLE_NAME = 'oa_trf_src'
 const REDUCED_TABLE_NAME = 'oa_trf_src_red'
+let colsToReduce = []
+let allCols = []
+let unmodifiedCols = []
+
+const getCols = () => {
+    return new Promise((resolve, reject) => {
+        db.all(`PRAGMA table_info(${SOURCE_TABLE_NAME})`, (err, rows) => {
+            if(err) reject(err)
+
+            colsToReduce = rows.map((row) => {
+                if(row.type.toLowerCase() !== 'integer'){
+                    return row.name
+                }
+            })
+
+            allCols = rows.map(row => row.name)
+
+            unmodifiedCols = allCols.filter((col) =>!colsToReduce.includes(col))
+
+            resolve()
+        })
+    })
+}
 
 /**
  * Fonction pour créer une table de correspondance
@@ -86,6 +109,34 @@ const createReducedTable = () => {
 
 const fillReducedTable = () => {
     return new Promise((resolve, reject) => {
+        db.all(`SELECT DISTINCT * FROM ${REDUCED_TABLE_NAME}`, (err, rows)=>{
+            if(err) reject(err)
+            const colNames = allCols.join(', ')
+            const colValues = allCols.map(colname => '?').join(', ')
+            const insertStmt = db.prepare(`INSERT INTO ${REDUCED_TABLE_NAME}(${colNames}) VALUES (${colValues})`)
 
+            rows.forEach(row => {
+                
+                const promises = colsToReduce.map((col)=> {
+                    return new Promise((resolve, reject) => {
+                        const tableName = `oa_trf_src_${col}_lkp`;
+                        db.get(`SELECT * FROM ${tableName} WHERE champ = ?`, [row[col]], (err, row) => {
+                            if(err) reject(err)
+                            resolve(row.id)
+                        })
+                    })
+                })
+
+                Promise.all(promises).then(ids => {
+                    const mappedValues = [...ids]
+                    unmodifiedCols.forEach(unmodifiedCol => {
+                        mappedValues.push(row[unmodifiedCol])
+                        insertStmt(mappedValues)
+                    })
+
+                })
+                insertStmt.finalize(resolve)
+            })
+        })
     })    
 }
